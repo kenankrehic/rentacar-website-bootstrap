fdb5f576ef8d060c1c86e158051118e54ee21e2d
Kenan Krehic <44552574+kenankrehic@users.noreply.github.com>
2018-11-18 14:59:56 +0100

    Added index styling and scripts

---
+++ script.js
@@ -0,0 +1,8 @@
+function sakrij(){
+	document.getElementById("auta").style.display = "block";
+}
+
+function onama(){
+	document.getElementById("onama").style.display = "block";
+	document.getElementById("auta").style.display = "none";
+}
